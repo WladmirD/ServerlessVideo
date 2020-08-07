6bb7be437fb8e93707544de9889471022bf1e754
WladmirD <wladmir.d@gmail.com>
2020-08-07 15:43:00 -0400

    Creando Navbar

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Routes from "./Routes";
+import { AppContext } from "./libs/contextLib";
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [user, setUser] = useState({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated, user, setUser }}>
+  <Routes />
+</AppContext.Provider>
     </div>
   );
 }
